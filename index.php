d1914bec2c6cf44b8ad6d974fce0ff0724bbb05f
Jesal Gadhia <jesal7@gmail.com>
2012-05-04 23:59:34 -0700

    More app logic

--- index.php
+++ index.php
@@ -13,14 +13,59 @@ $app = new Slim(array(
 ));
 
 //GET route
-$app->get('/index', function () {
+$app->get('/', function () {
     echo "Hello you've reached SlimJim!";
 });
 
 //POST route
-$app->post('/deploy/:payload', function ($payload) {
-    echo $payload;
-    //$app->response()->status(400);
+$app->post('/deploy', function () use ($app) {
+	
+	//TODO: Check for github's IP
+	$ip = $app->request()->getIp();
+	
+	//Get the repo or site URL
+
+	//
+
+	//Get the current branch
+	/*$stringfromfile = file(ROOT . '/.git/HEAD', FILE_USE_INCLUDE_PATH);
+	$stringfromfile = $stringfromfile[0]; 
+	$explodedstring = explode("/", $stringfromfile);
+	$current_branch = $explodedstring[2];*/
+
+	$payload = $app->request()->params('payload');
+
+    if(empty($payload)) {
+    	$app->halt(403);
+    }
+    
+    $payload = json_decode($payload); 
+
+    if(isset($payload->ref) && $payload->ref === 'refs/heads/develop') {
+
+    	//Check to see if repo is in the model
+    	//if($payload->repository)
+
+    	$commands = array(
+	        'whoami',
+	        /*'git fetch origin'
+	        'git reset --hard',
+	        'git rebase origin/develop develop',*/
+	        'git status',
+	    );
+
+	    $output = '';
+	    foreach($commands AS $command) {
+	        $tmp = shell_exec($command);
+	        $output .= "{$command}\n";
+	        $output .= htmlentities(trim($tmp)) . "\n";
+	    }
+	    
+	    echo $output;
+    	
+	} else {
+		$app->halt(400);
+	}
 });
 
 $app->run();

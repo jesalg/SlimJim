dbd4ade0e35255818052c85907efedd3749b2b44
Jesal Gadhia <jesal7@gmail.com>
2012-05-04 21:09:10 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+require 'Slim/Slim.php';
+
+//With default settings
+$app = new Slim();
+
+//With custom settings
+$app = new Slim(array(
+    'log.enable' => true,
+    'log.path' => './logs',
+    'log.level' => 4
+));
+
+//GET route
+$app->get('/index', function ($name) {
+    echo "Hello you've reached SlimJim!";
+});
+
+//POST route
+$app->post('/deploy/:payload', function () {
+    echo $payload
+    //$app->response()->status(400);
+});
+
+$app->run();

8484d392e538e106295daf0a73c7e8ea8386bd15
Ammar Hamilcar Tijani <ammar@tijani.nl>
2014-04-20 23:09:37 +0200

    complete bypass of cidr check (case: curl-php-apache-ssl server issues)

--- config.sample.php
+++ config.sample.php
@@ -8,16 +8,17 @@
 	class CUSTOM_CONFIG {
 		
 		/* Paths */
-		public static $ROOT_PATH				= '/srv/www/slimjim.yourcompany.com/public_html/';
+		public static $ROOT_PATH					= '/srv/www/slimjim.yourcompany.com/public_html/';
 						
 		/* MySQL */
-		public static $DB_NAME					= 'slimjim';
-		public static $DB_HOST					= 'localhost';
-		public static $DB_USER					= 'root';
-		public static $DB_PASS					= '';
-
-		/* CURL/SSL settings */
-		public static $CURL_SSL_VERIFICATION	= true;			//[true(default), false]. Bypass curl SSL verification, e.g. for a server with a self-signed SSL certificate.
+		public static $DB_NAME						= 'slimjim';
+		public static $DB_HOST						= 'localhost';
+		public static $DB_USER						= 'root';
+		public static $DB_PASS						= '';
+		
+		/* Github settings */
+		public static $GITHUB_CIDR_VERIFICATION		= true;			//[true(default), false]. Bypass CIDR(IP) verification of hook requests, e.g. in case of curl/ssl problems (alternative: restrict access via .htaccess).
+		public static $GITHUB_CURL_SSL_VERIFICATION	= true;			//[true(default), false]. Bypass curl SSL verification, e.g. for a server with a self-signed SSL certificate.
 	}
 	
 ?>

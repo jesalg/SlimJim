3255981141900b2a1d4b9ef9130a690b5a70c668
Ammar Hamilcar Tijani <ammar@tijani.nl>
2014-05-03 17:37:56 +0200

    Cleaned up config.sample.php, added warnings to curl/ssl settings

--- config.sample.php
+++ config.sample.php
@@ -8,17 +8,17 @@
 	class CUSTOM_CONFIG {
 		
 		/* Paths */
-		public static $ROOT_PATH					= '/srv/www/slimjim.yourcompany.com/public_html/';
+		public static $ROOT_PATH					= '/var/www/your.slimjim.path/';
 						
 		/* MySQL */
 		public static $DB_NAME						= 'slimjim';
 		public static $DB_HOST						= 'localhost';
-		public static $DB_USER						= 'root';
+		public static $DB_USER						= 'slimjim';
 		public static $DB_PASS						= '';
 		
 		/* Github settings */
-		public static $GITHUB_CIDR_VERIFICATION		= true;			//[true(default), false]. Bypass CIDR(IP) verification of hook requests, e.g. in case of curl/ssl problems (alternative: restrict access via .htaccess).
-		public static $GITHUB_CURL_SSL_VERIFICATION	= true;			//[true(default), false]. Bypass curl SSL verification, e.g. for a server with a self-signed SSL certificate.
+		public static $GITHUB_CIDR_VERIFICATION		= true;			//[true(default), false]. Bypass CIDR(IP) verification of hook requests, e.g. in case of curl/ssl problems (alternative: restrict access via .htaccess). WARNING: Use with caution as this setting value "false" introduces a potential security risk!
+		public static $GITHUB_CURL_SSL_VERIFICATION	= true;			//[true(default), false]. Bypass curl SSL verification, e.g. for a server with a self-signed SSL certificate. WARNING: Use with caution as this setting value "false" introduces a potential security risk!
 	}
 	
 ?>
